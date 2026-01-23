e89492f62b5d17c16fea9cba038b0df4e4e86ad7
xiuxiuu <hscheina@gmail.com>
2026-01-23 15:37:15 -0500

    Lecture 32
    non-void methods

--- TodoList/TodoList/Program.cs
+++ TodoList/TodoList/Program.cs
@@ -8,24 +8,53 @@ Console.WriteLine("[E]xit");
 
 //reads line from console and stores it in userInput variable
 var userInput = Console.ReadLine();
-if (userInput == "S" || userInput == "s")
+bool isLong = IsLong(userInput);
+
+var result = Add(5, 10);
+Console.WriteLine("10 + 5 = " + result);
+//if (userInput == "S" || userInput == "s")
+//{
+//    PrintSelectedOption("See all TODOs");
+//}
+//else if (userInput == "A" || userInput == "a")
+//{
+//    PrintSelectedOption("Add a TODO");
+//}
+//else if (userInput == "R" || userInput == "r")
+//{
+//    PrintSelectedOption("Remove a TODO");
+//}
+//else if (userInput == "E" || userInput == "e")
+//{
+//    PrintSelectedOption("Exit");
+//}
+Console.ReadKey(); //preventing window from closing immediately
+//void PrintSelectedOption(string selectedOption)
+//{
+//    Console.WriteLine("Selected option: " + selectedOption);
+//}
+
+int Add(int a, int b)
 {
-    PrintSelectedOption("See all TODOs");
+    //if (a > 10)
+    //{
+    //    return a + b; // error - not all code paths return a value
+    //}
+    return a + b;
 }
-else if (userInput == "A" || userInput == "a")
+
+bool IsLong(string input)
 {
-    PrintSelectedOption("Add a TODO");
-}
-else if (userInput == "R" || userInput == "r")
-{
-    PrintSelectedOption("Remove a TODO");
-}
-else if (userInput == "E" || userInput == "e")
-{
-    PrintSelectedOption("Exit");
-}
-    Console.ReadKey(); //preventing window from closing immediately
-void PrintSelectedOption(string selectedOption)
-{
-    Console.WriteLine("Selected option: " + selectedOption);
+    //if(input.Length > 10)
+    //{
+    //    return true;
+    //}
+    ////else
+    ////{
+    ////    return false;
+    ////}
+    //return false;
+
+    return input.Length > 10;
+    //code refactoring - the process of improving the code quality without altering its behavior
 }
--- TodoList/TodoList/obj/Debug/net10.0/TodoList.AssemblyInfo.cs
+++ TodoList/TodoList/obj/Debug/net10.0/TodoList.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d4347965c2c459a5461adaa96c257c1f896d12b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bac476c615905536fd1206b1d81d1ba134b1658")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
