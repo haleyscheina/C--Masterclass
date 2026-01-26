6cdfe7857086ebb9c5c03a55650e729b2219b0b7
xiuxiuu <hscheina@gmail.com>
2026-01-26 18:36:31 -0500

    Coding exercise 7

---
+++ While loop - CalculateSumOfNumbersBetween - Coding exercise/While loop - CalculateSumOfNumbersBetween - Coding exercise/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Coding.Exercise
+{
+    public class Exercise
+    {
+        public static int CalculateSumOfNumbersBetween(int firstNumber, int lastNumber)
+        {
+            if (lastNumber < firstNumber)
+            {
+                return 0;
+            }
+
+            int sum = 0;
+            while (firstNumber <= lastNumber)
+            {
+                sum += firstNumber;
+                firstNumber++;
+            }
+            return sum;
+        }
+    }
+}
