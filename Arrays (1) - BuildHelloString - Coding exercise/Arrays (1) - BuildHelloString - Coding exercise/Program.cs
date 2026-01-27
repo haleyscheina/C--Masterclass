90f4bb99662f4c9eb311d9852be2952be92ec839
xiuxiuu <hscheina@gmail.com>
2026-01-27 16:19:29 -0500

    Coding exercise 10

---
+++ Arrays (1) - BuildHelloString - Coding exercise/Arrays (1) - BuildHelloString - Coding exercise/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Arrays (1) - BuildHelloString
+
+The purpose of the BuildHelloString is to iterate an array of chars consisting of all letters of the word "hello" and add those letters to the result string variable, so at the end of this method, this variable is equal to "hello".
+
+
+*/
+using System;
+
+namespace Coding.Exercise
+{
+    public class Exercise
+    {
+        public static string BuildHelloString()
+        {
+            char[] letters = new char[] { 'h', 'e', 'l', 'l', 'o' };
+            var result = "";
+            for (int i = 0; i < letters.Length; ++i)
+            {
+                result += letters[i];
+            }
+            return result;
+        }
+    }
+}
