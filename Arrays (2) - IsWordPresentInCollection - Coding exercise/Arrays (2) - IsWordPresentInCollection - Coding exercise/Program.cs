a9ff699d92db8fbd4474a0c662b5e0ef78893969
xiuxiuu <hscheina@gmail.com>
2026-01-27 16:25:19 -0500

    Coding exercise 11

---
+++ Arrays (2) - IsWordPresentInCollection - Coding exercise/Arrays (2) - IsWordPresentInCollection - Coding exercise/Program.cs
@@ -0,0 +1,31 @@
+﻿/* Arrays (2) - IsWordPresentInCollection
+
+Implement the IsWordPresentInCollection method, which given an array of words and a wordToBeChecked, returns true if wordToBeChecked is present in this collection and false otherwise.
+
+For example:
+
+    For words equal to {"one", "two", "three"} and wordToBeChecked equal to "hello" the result shall be false because the word "hello" is not present in the words array.
+
+    For words equal to {"one", "two", "three"} and wordToBeChecked equal to "two" the result shall be true because the word "two" is present in the words array.
+*/
+
+using System;
+
+namespace Coding.Exercise
+{
+    public class Exercise
+    {
+        public static bool IsWordPresentInCollection(string[] words, string wordToBeChecked)
+        {
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i] == wordToBeChecked)
+                {
+                    return true;
+                    break;
+                }
+            }
+            return false;
+        }
+    }
+}
