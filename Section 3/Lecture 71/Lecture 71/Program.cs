6f03ddf498f2e76d0aeb642e05b8f44760f9af8e
xiuxiuu <hscheina@gmail.com>
2026-01-29 16:33:44 -0500

    Lecture 72

--- Section 3/Lecture 71/Lecture 71/Program.cs
+++ Section 3/Lecture 71/Lecture 71/Program.cs
@@ -3,10 +3,26 @@ var rectangle1 = new Rectangle();
 Console.WriteLine($"Width is: {rectangle1.width}");
 Console.WriteLine($"Height is: {rectangle1.height}");
 
+/*access modifiers
+ * - private members can only bw accessed within the class it belongs to
+ * - public members can be accessed everywhere
+ */
+
 Console.ReadKey();
 
 class Rectangle
 {
-    int width;
-    int height;
+    //default access modifier is private, hence "'Rectangle.width' is inaccessible due to its protection level" error
+    //int width;
+    //int height;
+
+    //in order to make width and height accessible outside of the Rectangle class, add public access modifier
+    public int width;
+    public int height;
+
+    void DummyMethod()
+    {
+        //works because code belongs to Rectangle class
+        Console.WriteLine($"Height is {height}");
+    }
 }
