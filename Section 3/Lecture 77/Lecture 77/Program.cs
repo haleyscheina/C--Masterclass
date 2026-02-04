8775518b59a4ac7405cdbafe5b23a85e9f43e6b9
xiuxiuu <hscheina@gmail.com>
2026-02-04 13:19:45 -0500

    Lecture 80

--- Section 3/Lecture 77/Lecture 77/Program.cs
+++ Section 3/Lecture 77/Lecture 77/Program.cs
@@ -1,32 +1,32 @@
-﻿//Method overloading
+﻿//"this" keyword
+using System.Net.Http.Headers;
+
 var medicalAppointment = new MedicalAppointment("John Smith", new DateTime(2023, 4, 3));
-
-//overwrite month and day
-//medicalAppointment.Reschedule(5, 1);
 medicalAppointment.OverwriteMonthAndDay(5, 1);
-
-//adds a given number of months and days
-//medicalAppointment.Reschedule(1, 2); 
 medicalAppointment.MoveByMonthsAndDays(1, 2);
 
 Console.ReadKey();
-
+class MedicalAppointmentPrinter
+{
+    public void Print(MedicalAppointment medicalAppointment)
+    {
+        Console.WriteLine($"Appointment will take place on {medicalAppointment.GetDate()}");
+    }
+}
 class MedicalAppointment
 {
-    private string _patientName;
+    private string patientName;
     private DateTime _date;
 
     public MedicalAppointment(string patientName, DateTime date)
     {
-        _patientName = patientName;
+        this.patientName = patientName;
         _date = date;
     }
-    //call one constructor from another using the "this" keyword
-    //in this case, it avoids code duplication as the constructors do essentially the same thing, only one has a fixed value of 7 added days
+
+    public DateTime GetDate() => _date;
     public MedicalAppointment(string patientName) : this(patientName, 7)
     {
-        //_patientName = patientName;
-        //_date = DateTime.Now.AddDays(7);
     }
     public MedicalAppointment(string patientName, int daysFromNow)
     {
@@ -36,23 +36,13 @@ class MedicalAppointment
     public void Reschedule(DateTime date)
     {
         _date = date;
+        var printer = new MedicalAppointmentPrinter();
+        printer.Print(this);
     }
-
-    //as long as the compiler can tell the difference by type, count, or order of parameters, it is fine
-    //always make sure overridden methods are distinguishable 
-    //public void Reschedule(int month, int day)
-    //{
-    //    _date = new DateTime(_date.Year, month, day);
-    //}
     public void OverwriteMonthAndDay(int month, int day)
     {
         _date = new DateTime(_date.Year, month, day);
     }
-
-    //public void Reschedule(int monthsToAdd, int daysToAdd)
-    //{
-    //    _date = new DateTime(_date.Year, _date.Month + monthsToAdd, _date.Day + daysToAdd);
-    //}
     public void MoveByMonthsAndDays(int monthsToAdd, int daysToAdd)
     {
         _date = new DateTime(_date.Year, _date.Month + monthsToAdd, _date.Day + daysToAdd);
